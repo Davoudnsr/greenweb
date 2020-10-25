621c2de5079b9c5f5412d39f8d6872b727ab416c
Davoudnsr <davoud.nasrabadi@gmail.com>
2020-10-26 00:28:01 +0330

    nothing added

--- index.js
+++ index.js
@@ -11,5 +11,34 @@ function average(arr) {
   let avg = sum / arr.length;
   return avg;
 }
+function mapSum(arr, value) {
+  let x = [];
+  let index = 0;
+  for (let i of arr) {
+    x[index] = i + value;
+    index++;
+  }
+  return x;
+}
+function mapSub(arr, value) {
+  let x = [];
+  let index = 0;
+  for (let i of arr) {
+    x[index] = i - value;
+    index++;
+  }
+  return x;
+}
+function map(arr, action, value) {
+  switch (action) {
+    case "+":
+      return mapSum(arr, value);
+    case "-":
+      return mapSub(arr, value);
+    default:
+      return arr;
+  }
+}
 module.exports.reduce = reduce;
 module.exports.average = average;
+module.exports.map = map;
